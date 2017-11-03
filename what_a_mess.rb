06b8f5d1dd2fead2149b4ce1902692be0ddcee19
chris fuentes <chris.fuentes@ellucian.com>
2017-11-03 11:45:26 -0400

    gasp, was using outdated SDK

--- what_a_mess.rb
+++ what_a_mess.rb
@@ -4,10 +4,10 @@ require 'twilio-ruby'
 
 account_sid = ENV['twilio_account_sid']
 auth_token = ENV['twilio_auth_token']
-@client = Twilio::REST::Client.new account_sid, auth_token
+@client = Twilio::REST::Client.new(account_sid, auth_token)
 
-message = @client.accounts.message.create(
+@message = @client.messages.create(
   :body => ENV['twilio_msg_body'],
   :to => ENV['alert_number'],
   :from => ENV['twilio_number'])
-puts message.to
+puts @message.status
